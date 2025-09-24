9248340e620fccf8b42c0ef249a82d62641c1ffa
Svend <svc001@edu.zealand.dk>
2025-09-24 12:02:12 +0200

    Man kan nu lave opgaver, som gemmes på en database og vises kun hos nødvendige brugere

--- index.php
+++ index.php
@@ -16,6 +16,24 @@ $userId = $_SESSION['userId'];
 $user = $db->sql("SELECT name FROM users WHERE userId = :userId", [":userId" => $userId]);
 $username = $user[0]->name;
 
+if (!empty($_POST["data"])) {
+    $data = $_POST['data'];
+
+    $sql = "INSERT INTO tasks (taskUserId, title, description, type) 
+            VALUES (:taskUserId, :title, :description, :type)";
+
+    $bind = [
+        ":taskUserId"   => $userId,  // comes from session
+        ":title"        => $data["title"],
+        ":description"  => $data["description"],
+        ":type"         => $data["type"]
+    ];
+
+    $db->sql($sql, $bind, false);
+    header("Location: index.php");
+    exit;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="da">
@@ -29,6 +47,7 @@ $username = $user[0]->name;
     <meta name="copyright" content="Information om copyright">
     
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="favicon2.jpg">
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -61,20 +80,41 @@ $username = $user[0]->name;
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Lav en opgave</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
+            <form action="index.php" method="post">
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Lav en opgave</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <label for="title" class="form-label">Task</label>
+                    <input type="text" class="form-control" id="title" name="data[title]" placeholder="Task" value="" required>
+
+                    <label for="description" class="form-label mt-2">Description</label>
+                    <textarea class="form-control" placeholder="Efterlad en deskriptorer til opgaven" id="description" name="data[description]"></textarea>
+
+                    <label for="type" class="form-label mt-2">Opgave Type</label>
+                    <select name="data[type]" id="type" class="form-select" required>
+                        <option value="">--Vælg Type--</option>
+                        <option value="daily">Daglig</option>
+                        <option value="todo">To-Do</option>
+                        <option value="habit">vane</option>
+                    </select>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuller</button>
+                    <button type="submit" class="btn btn-primary">Opret opgave</button>
+                </div>
+
+                <input type="hidden" name="questId" value="4">
+            </form>
         </div>
     </div>
 </div>
+
 
 <!-- Example Task Con -->
 <div class="container-fluid">
@@ -84,26 +124,33 @@ $username = $user[0]->name;
             <label for="dailies" class="fs-5">Daglige</label>
             <div id="dailies" class="bg-white rounded-4 p-2">
 
-                <div class="d-flex border border-light border-2 rounded-4 p-1 m-2">
-                    <div class="check-bg bg-primary rounded-4">
-                        <div class="check-box rounded-4"></div>
-                    </div>
+                <?php
+                $dailies = $db->sql('SELECT * FROM tasks WHERE type = "daily" AND taskUserId = :userId', [":userId" => $userId]);
+                foreach ($dailies as $daily) {
+                    ?>
+                    <div class="d-flex border border-light border-2 rounded-4 p-1 m-2">
+                        <div class="check-bg bg-primary rounded-4">
+                            <div class="check-box rounded-4"></div>
+                        </div>
 
-                    <div class="ms-3">
-                        <p class="m-0 fs-3 fw-bold">
-                            Daily name
-                        </p>
-                        <p class="m-0">
-                            Daily desc
-                        </p>
-                    </div>
+                        <div class="ms-3">
+                            <?php
+                            echo "<p class='m-0 fs-3 fw-bold'>" . $daily->title . "</p>";
+                            ?>
+                            <?php
+                            echo "<p class='m-0'>" . $daily->description . "</p>";
+                            ?>
+                        </div>
 
-                    <div class="align-self-center ms-auto">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-                        </svg>
+                        <div class="ms-auto align-self-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                            </svg>
+                        </div>
                     </div>
-                </div>
+                    <?php
+                }
+                ?>
 
             </div>
         </div>
@@ -111,19 +158,22 @@ $username = $user[0]->name;
         <div class="col-12 col-md-3">
             <label for="To-Do" class="fs-5">To-Do</label>
             <div id="To-Do" class="bg-white rounded-4 p-2">
-
+                <?php
+                $todos = $db->sql('SELECT * FROM tasks WHERE type = "todo" AND taskUserId = :userId', [":userId" => $userId]);
+                foreach ($todos as $todo) {
+                ?>
                 <div class="d-flex border border-light border-2 rounded-4 p-1 m-2">
                     <div class="check-bg bg-secondary rounded-4">
                         <div class="check-box rounded-4"></div>
                     </div>
 
                     <div class="ms-3">
-                        <p class="m-0 fs-3 fw-bold">
-                            To-Do name
-                        </p>
-                        <p class="m-0">
-                            To-Do desc
-                        </p>
+                        <?php
+                        echo "<p class='m-0 fs-3 fw-bold'>" . $todo->title . "</p>";
+                        ?>
+                        <?php
+                        echo "<p class='m-0'>" . $todo->description . "</p>";
+                        ?>
                     </div>
 
                     <div class="ms-auto align-self-center">
@@ -132,26 +182,31 @@ $username = $user[0]->name;
                         </svg>
                     </div>
                 </div>
-
+                <?php
+                    }
+                ?>
             </div>
         </div>
         <!-- Habits -->
         <div class="col-12 col-md-3">
             <label for="habits" class="fs-5">Vaner</label>
             <div id="habits" class="bg-white rounded-4 p-2">
-
+                <?php
+                $habits = $db->sql('SELECT * FROM tasks WHERE type = "habit" AND taskUserId = :userId', [":userId" => $userId]);
+                foreach ($habits as $habit) {
+                ?>
                 <div class="d-flex border border-light border-2 rounded-4 p-1 m-2">
                     <div class="check-bg bg-info rounded-4">
                         <div class="check-box rounded-4 d-flex justify-content-center align-items-center"><span class="fs-1 text-center">+</span></div>
                     </div>
 
                     <div class="ms-3">
-                        <p class="m-0 fs-3 fw-bold">
-                            Habit Name
-                        </p>
-                        <p class="m-0">
-                            Habit Desc
-                        </p>
+                        <?php
+                        echo "<p class='m-0 fs-3 fw-bold'>" . $habit->title . "</p>";
+                        ?>
+                        <?php
+                        echo "<p class='m-0'>" . $habit->description . "</p>";
+                        ?>
                     </div>
 
                     <div class="d-flex ms-auto">
@@ -166,10 +221,16 @@ $username = $user[0]->name;
                         </div>
                     </div>
                 </div>
-
+                <?php
+                    }
+                ?>
             </div>
         </div>
     </div>
+</div>
+
+<div class="text-center mt-5">
+    <a href="logout.php">log ud</a>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
